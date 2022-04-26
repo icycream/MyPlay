2a57e04a710c2620b54989d462111e10707e9bb6
icycream <napasploy@gmail.com>
2022-04-26 14:13:45 +0700

    Initial Commit

---
+++ MyPlay/MyPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlayApp.swift
+//  MyPlay
+//
+//  Created by IcyCream on 26/4/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct MyPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
